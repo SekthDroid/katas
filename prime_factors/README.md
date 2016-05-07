864a8db66d6762da501a6d145879bb08c521cdcf
brhmms <contact@abrahammesa.com>
2016-05-07 20:54:12 +0200

    prime factors

---
+++ prime_factors/README.md
@@ -0,0 +1,1 @@
+http://www.butunclebob.com/ArticleS.UncleBob.ThePrimeFactorsKata
---
+++ prime_factors/prime_factors.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace brhmms\katas;
+
+class PrimeFactors {
+
+    /**
+     * Returns prime factors in numerical sequence
+     * @param int $number
+     * @return array of integers
+     */
+    public function generate(int $number) {
+        $numbers_list = [];
+        for ($maybe_prime = 2; $number > 1; $maybe_prime++) {
+            for (; $number % $maybe_prime === 0; $number /= $maybe_prime) {
+                $numbers_list[] = $maybe_prime;
+            }
+        }
+        return $numbers_list;
+    }
+
+}
