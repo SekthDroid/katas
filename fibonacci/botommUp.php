2cb3387c4d87117bf82f360c0a820df5f5fb77a2
David Sastre <secthndamon@gmail.com>
2016-05-11 13:45:06 +0200

    Changed $fib variable name to $sequence, for better readability

--- fibonacci/botommUp.php
+++ fibonacci/botommUp.php
@@ -6,18 +6,18 @@ use brhmms\katas\Fibonacci;
 
 class BottomUp implements Fibonacci {
 
-    private $fib = array();
-    
+    private $sequence = array();
+
     public function fib(int $number) {
         for ($k = 1; $k <= $number; $k++) {
             if ($k <= 2) {
                 $f = 1;
             } else {
-                $f = $this->fib[$k - 1] + $this->fib[$k - 2];
+                $f = $this->sequence[$k - 1] + $this->sequence[$k - 2];
             }
-            $this->fib[$k] = $f;
+            $this->sequence[$k] = $f;
         }
-        return $this->fib[$number];
+        return $this->sequence[$number];
     }
 
 }
